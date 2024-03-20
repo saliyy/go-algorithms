b4662157b69516ce116d3bd03ddd4353ea1cf6f9
Vladislav Saliy <saliyvls@yandex.ru>
2024-03-20 12:24:01 +0300

    ConvertStortedArrayToBST

---
+++ ConvertStortedArrayToBST/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	nums := []int{-10, -3, 0, 5, 9}
+
+	fmt.Println(sortedArrayToBST(nums))
+}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+
+	var helper func(l, r int) *TreeNode
+
+	helper = func(l, r int) *TreeNode {
+
+		if l <= r {
+
+			mid := (l + r) / 2
+
+			node := &TreeNode{Val: nums[mid]}
+
+			node.Left = helper(l, mid-1)
+			node.Right = helper(mid+1, r)
+
+			return node
+		}
+
+		return nil
+	}
+
+	return helper(0, len(nums)-1)
+}
